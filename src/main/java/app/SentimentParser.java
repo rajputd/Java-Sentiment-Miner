5e0edeaeb1a5dc9462a350db1232b25f8a49ad03
rajputd <dileep_rajput@yahoo.com>
2018-11-14 19:04:14 -0500

    Impliment loadWords method in SentimentParser

--- src/main/java/app/SentimentParser.java
+++ src/main/java/app/SentimentParser.java
@@ -1,5 +1,9 @@
 package app;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashSet;
 
 enum Sentiments {POSITIVE, NEGATIVE, NEUTRAL}
@@ -9,8 +13,12 @@ public class SentimentParser {
     private HashSet<String> positiveWords;
     private HashSet<String> negativeWords;
 
-    public SentimentParser(String posLexiconLocation, String negLexiconLocation) {
-        this.loadWords(posLexiconLocation, negLexiconLocation);
+    public SentimentParser(String posLexiconLocation, String negLexiconLocation) throws Exception {
+        this.positiveWords = new HashSet<>();
+        this.negativeWords = new HashSet<>();
+
+        this.loadWords(this.positiveWords, posLexiconLocation);
+        this.loadWords(this.negativeWords, negLexiconLocation);
     }
 
 
@@ -24,20 +32,30 @@ public class SentimentParser {
 
     /*loadWords*/
     /**
-     * Loads the lexicons associated with posLexicon and negLexiconLocation in the positiveWordsLocation
-     * and negativeWords HashSets. This must be done before the parseSentiment method is used. Otherwise an error will
-     * be thrown.
-     * @param posLexiconLocation String that points to the location of the file containing the positive words lexicon.
-     * @param negLexiconLocation String that points to the location of the file containing the negative words lexicon.
+     * Loads the lexicon of words associated with the file at LexiconLocation into wordsSet.
+     *
+     * @param wordsSet HashSet<String> that is the set that will store the words at LexiconLocation.
+     * @param lexiconLocation String that points to the location of the file containing the lexicon.
      */
-    void loadWords(String posLexiconLocation, String negLexiconLocation) {
+    void loadWords(HashSet<String> wordsSet, String lexiconLocation) throws IOException {
 
+        File file = new File(lexiconLocation);
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            if (line.length() == 0 || line.charAt(0) == ';' || line.charAt(0) == '\n') {
+                continue;
+            }
+
+            wordsSet.add(line);
+        }
     }
 
     /**
      * Analyzes the given phrase using the preloaded lexicons and returns the sentiment associated with the phrase.
-     * @param phrase String that contains data that contains the sentiment that will be parsed
-     * @return The sentiment (POSITIVE, NEGATIVE, or NEUTRAL) associated with the given phrase
+     * @param phrase String that contains data that contains the sentiment that will be parsed.
+     * @return The sentiment (POSITIVE, NEGATIVE, or NEUTRAL) associated with the given phrase.
      */
     Sentiments parseSentiment(String phrase) {
         return Sentiments.NEUTRAL;
