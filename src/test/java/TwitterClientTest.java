af47b7476ee92497f32a2f184d58f65bdad551f4
rajputd <dileep_rajput@yahoo.com>
2018-11-15 01:13:00 -0500

    Add tests for soon to be made TwitterClient class

---
+++ src/test/java/TwitterClientTest.java
@@ -0,0 +1,23 @@
+import app.TwitterClient;
+import org.junit.Assert;
+import org.junit.Test;
+import twitter4j.Status;
+
+import java.util.List;
+
+public class TwitterClientTest {
+    @Test
+    public void twitterClientSmokeTest() {
+        List<Status> result = null;
+
+        try {
+            result = TwitterClient.getTweets("#Trump", 100 );
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+        Assert.assertTrue(result != null);
+    }
+
+}
