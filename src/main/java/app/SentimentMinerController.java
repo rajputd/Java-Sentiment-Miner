8b196805995880d58d4d0bb884b84009db6a51c8
rajputd <dileep_rajput@yahoo.com>
2018-11-15 01:44:04 -0500

    Set up application routes and skeleton front-end

--- src/main/java/app/SentimentMinerController.java
+++ src/main/java/app/SentimentMinerController.java
@@ -3,14 +3,22 @@ package app;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SentimentMinerController {
 
     @GetMapping("/")
-    public String displaySentimentData(@RequestParam(name="hashtag", required=false) String hashtag, Model model) {
-        model.addAttribute("hashtag", hashtag);
+    public String displayHomePage() {
+        return "home";
+    }
+
+    @PostMapping("/")
+    public String displaySentimentData(@RequestParam(name="hashtag", required = true) String hashtag, Model model) {
+
+        String dataset = "{positive: 10, negative: 30, neutral: 60 }";
+        model.addAttribute("dataset", dataset);
         return "minerDisplay";
     }
 
